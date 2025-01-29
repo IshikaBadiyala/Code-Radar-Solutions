85dda5646b8e5c641becc66a6efd4e2970a54e84
IshikaBadiyala <ishika1174.becse24@chitkara.edu.in>
2025-01-29 17:07:16 +0530

    Solution to Check Month Days

--- Check Month Days@@6788dd63490637cfc44e1baa/code.c
+++ Check Month Days@@6788dd63490637cfc44e1baa/code.c
@@ -4,18 +4,18 @@ int main()
 {
     int a;
     scanf("%d",&a);
-    if(a==1){printf("%d\n",31)};
-    else if(a==2){printf("%d\n",28)};
-    else if(a==3){printf("%d\n",31)};
-    else if(a==4){printf("%d\n",30)};
-    else if(a==5){printf("%d\n",31)};
-    else if(a==6){printf("%d\n",30)};
-    else if(a==7){printf("%d\n",31)};
-    else if(a==8){printf("%d\n",31)};
-    else if(a==9){printf("%d\n",30)};
-    else if(a==10){printf("%d\n",31)};
-    else if(a==11){printf("%d\n",30)};
-    else if(a==12){printf("%d\n",31)};
+    if(a==1){printf("%d\n",31);}
+    else if(a==2){printf("%d\n",28);}
+    else if(a==3){printf("%d\n",31);}
+    else if(a==4){printf("%d\n",30);}
+    else if(a==5){printf("%d\n",31);}
+    else if(a==6){printf("%d\n",30);}
+    else if(a==7){printf("%d\n",31);}
+    else if(a==8){printf("%d\n",31);}
+    else if(a==9){printf("%d\n",30);}
+    else if(a==10){printf("%d\n",31);}
+    else if(a==11){printf("%d\n",30);}
+    else if(a==12){printf("%d\n",31);}
     else{
         printf("Invalid month");
     }
