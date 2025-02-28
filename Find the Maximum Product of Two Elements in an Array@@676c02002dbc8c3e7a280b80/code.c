527516ed07a18e2f30c5760c431d6216fc767f0e
IshikaBadiyala <ishikabadiyala05@gmail.com>
2025-02-28 13:19:29 +0530

    Solution to Find the Maximum Product of Two Elements in an Array

--- Find the Maximum Product of Two Elements in an Array@@676c02002dbc8c3e7a280b80/code.c
+++ Find the Maximum Product of Two Elements in an Array@@676c02002dbc8c3e7a280b80/code.c
@@ -1,34 +1,37 @@
-#include<stdio.h>
-#include<limits.h>
+#include <stdio.h>
+#include <limits.h>
 
-
-int main()
-{
-    int i,j,largest=INT_MIN,secondlarg= INT_MIN,isfound=0;
-    scanf("%d",&i);
+int main() {
+    int i, j, largest = INT_MIN, secondlarg = INT_MIN, isfound = 0;
+    scanf("%d", &i);
     int arr[i];
-    for(j=0;j<i;j++)
-    {
-        scanf("%d",&arr[j]);
-    }
- 
-    for(j=1;j<i;j++)
-    {
-        if(largest<arr[j])
-        {
-           largest=arr[j];
-        }  
+    for (j = 0; j < i; j++) {
+        scanf("%d", &arr[j]);
     }
 
-    for(j=0;j<i;j++)
-    {
-        if(arr[j]>secondlarg &&arr[j]!=largest)
-       { secondlarg=arr[j];
-        isfound=1;
-       }
+    if (i < 2) {
+        printf("-1\n");
+        return 0;
     }
-    if(isfound)
-    printf("%d\n",largest*secondlarg);
-  else
-  printf("-1\n");
+
+    for (j = 0; j < i; j++) {
+        if (arr[j] > largest) {
+            largest = arr[j];
+        }
+    }
+
+    for (j = 0; j < i; j++) {
+        if (arr[j] > secondlarg && arr[j] != largest) {
+            secondlarg = arr[j];
+            isfound = 1;
+        }
+    }
+
+    if (isfound) {
+        printf("%d\n", largest * secondlarg);
+    } else {
+        printf("-1\n");
+    }
+
+    return 0;
 }
