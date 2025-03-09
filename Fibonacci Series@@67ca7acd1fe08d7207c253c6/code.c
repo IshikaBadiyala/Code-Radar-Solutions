f04650ea607ef4daa487e37f55584da10842d0c1
IshikaBadiyala <ishikabadiyala05@gmail.com>
2025-03-09 11:39:34 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,8 +1,11 @@
  int fibonacciSeries(int n)
  {
-    int a,c,b;
+    int a=0,c,b=1;
     if(n==1)
+  {
+    printf("%d",a);
     return 0;
+  }
        for (int i=1;i<=n;i++)
    {
       printf("%d",a);
