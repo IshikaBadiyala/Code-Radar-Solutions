7661f7a30f62837ca2a7ee50b86450d41dbe046a
IshikaBadiyala <ishikabadiyala05@gmail.com>
2025-03-30 00:00:07 +0530

    Solution to Factorial Range

---
+++ Factorial Range@@67de00956ac3fec9afb90e10/code.c
@@ -0,0 +1,13 @@
+int factorialRange(int start,int end)
+{
+    int i,j;
+    for (i=start;i<end;i++)
+    {
+        int fact=1;
+       for(i=1;i<=end;i++)
+       {
+        fact=fact*i;
+        return fact;
+       }  
+    }
+}
