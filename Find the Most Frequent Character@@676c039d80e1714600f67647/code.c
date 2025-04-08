9bef85959f1b3ba41cbb1655aa213fc47ff2c286
IshikaBadiyala <ishikabadiyala05@gmail.com>
2025-04-08 23:19:29 +0530

    Solution to Find the Most Frequent Character

--- Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
+++ Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
@@ -1,30 +1,34 @@
 #include<stdio.h>
 #include<string.h>
-#include<limits.h>
-int main()
-{
+
+int main() {
     char str[100];
-    fgets(str,sizeof(str),stdin);
-    str[strcspn(str,"\n")]='\0';
-    int max_freq=0;
-    int max_element;
+    fgets(str, sizeof(str), stdin); 
+    str[strcspn(str, "\n")] = '\0'; // Remove newline character
+
+    int max_freq = 0; 
+    char max_element;
     int count1;
-    int count=strlen(str);
-    for(int i=0;i<count;i++)
-    {
-        count1=1;
-        for(int j=i+1;j<count;j++)
-        {
-            if(str[i]==str[j])
-            {
-                count1+=1;
+    int count = strlen(str);
+
+    for (int i = 0; i < count; i++) {
+        if (str[i] == ' ') continue; // Skip spaces
+
+        count1 = 1; // Start counting from the current character itself
+
+        for (int j = i + 1; j < count; j++) {
+            if (str[i] == str[j]) {
+                count1++;
             }
         }
-        if(count1>max_freq)
-        {
-            max_freq=count1;
-            max_element=str[i];
+
+        // Ensure first occurrence priority
+        if (count1 > max_freq || (count1 == max_freq && max_element > str[i])) {
+            max_freq = count1;
+            max_element = str[i];
         }
     }
-    printf("%c",max_element);
+
+    printf("Most frequent character: %c\n", max_element);
+    return 0;
 }
