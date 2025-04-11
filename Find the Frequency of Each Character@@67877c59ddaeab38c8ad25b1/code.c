d6c19e7c798028559277d39811fcbae1339269fa
IshikaBadiyala <ishikabadiyala05@gmail.com>
2025-04-11 11:38:05 +0530

    Solution to Find the Frequency of Each Character

--- Find the Frequency of Each Character@@67877c59ddaeab38c8ad25b1/code.c
+++ Find the Frequency of Each Character@@67877c59ddaeab38c8ad25b1/code.c
@@ -1,24 +1,33 @@
-#include<stdio.h>
-#include<string.h>
-#define MAX_CHAR 256
-int main()
-{
+#include <stdio.h>
+#include <string.h>
+
+int main() {
     char str[100];
-    int freq[MAX_CHAR]={0};
-    fgets(str,sizeof(str),stdin);
-    for(int i=0;i<strlen(str);i++)
-    {
-        if(str[i]!='\0')
-        {
-            freq[str[i]]++;
+    fgets(str, sizeof(str), stdin);
+    
+    int length = strlen(str);
+    
+    // Outer loop iterates over each character
+    for (int i = 0; i < length; i++) {
+        if (str[i] == ' ' || str[i] == '\n') {  // Ignore spaces and newlines
+            continue;
+        }
+
+        int count = 1;  // Initialize count for str[i]
+
+        // Inner loop checks occurrences of str[i] in the rest of the string
+        for (int j = i + 1; j < length; j++) {
+            if (str[j] == str[i]) {
+                count++;
+                str[j] = '0'; // Mark character as counted to avoid duplicate printing
+            }
+        }
+
+        // Print the character only if it's not marked as counted
+        if (str[i] != '0') {
+            printf("%c: %d\n", str[i], count);
         }
     }
-    for(int i=0;i<strlen(str);i++)
-    {
-        if(freq[str[i]]>0)
-        {
-            printf("%c: %d\n",str[i],freq[str[i]]);
-            freq[str[i]]=0;
-        }
-    }
+
+    return 0;
 }
